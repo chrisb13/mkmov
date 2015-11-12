f29a6480c4d1011549621fd4f23e5eb4b19fc74e
Christopher Bull <z3457920@student.unsw.edu.au>
2015-11-12 22:03:53 +1100

    refactoring to now include MovMaker Class and change of argument order, can now accept a globbed list of files

--- main.py
+++ main.py
@@ -18,18 +18,21 @@
 
 
 """
-MkMov v0.1
-This is a python script for making movies from a netCDF file.
+MkMov v0.2
+This is a python script for making movies from a netCDF file. Interface is by command line.
 
-Example: python main.py /srv/ccrc/data42/z3457920/20151012_eac_sep_dynamics/nemo_cordex24_ERAI01/1989/cordex24-ERAI01_1d_19890101_19890105_grid_T_2D.nc tos
+Examples: 
+python main.py --help
+python /srv/ccrc/data42/z3457920/20151012_eac_sep_dynamics/nemo_cordex24_ERAI01/1989/cordex24-ERAI01_1d_19890101_19890105_grid_T_2D.nc tos main.py 
+python /srv/ccrc/data42/z3457920/20151012_eac_sep_dynamics/nemo_cordex24_ERAI01/*/cordex24-ERAI01_1d_*_grid_T_2D.nc tos main.py 
 
 Usage:
     main.py -h
-    main.py FILE_NAME VARIABLE_NAME 
+    main.py VARIABLE_NAME FILE_NAME...
 
 Arguments:
-    FILE_NAME       path to NetCDF file to make movie
     VARIABLE_NAME   variable name
+    FILE_NAME       path to NetCDF file to make movie, can also be a list of files (dimensions must be the same)
 
 Options:
     -h,--help          : show this help message
@@ -121,7 +124,8 @@ def dispay_passed_args(workingfolder):
     lg.info("-----------------------------------------------------------------")
     lg.info("MkMov has been run with the following options...")
 
-    lg.info("File we are making a movie of: "+ os.path.basename(arguments['FILE_NAME']))
+    for f in arguments['FILE_NAME']:
+        lg.info("File we are making a movie of file(s): "+ os.path.basename(f))
 
     lg.info("Variable we are making a movie of: "+ arguments['VARIABLE_NAME'])
 
@@ -137,62 +141,143 @@ def dispay_passed_args(workingfolder):
     return
 
 
-def create_plot(name_of_array,timedimen,workingfolder):
-    """function to create plots.
-    
-    :name_of_array: @todo
-    :timedimen: @todo
+
+class MovMaker(object):
+    """
+    Class to create movie based on file list and variable name.
+
+    Parameters
+    ----------
+    :filelist:
+    variable_name: 
     :workingfolder: @todo
-    :returns: @todo
+
+    Returns
+    -------
+    
+    Notes
+    -------
+    
+
+    Example
+    --------
+    >>> 
+    >>> 
     """
 
-    plt.close('all')
-    fig=plt.figure()
+    def __init__(self, filelist,variable_name,workingfolder):
+        #super(MovMaker, self).__init__()
+        self.filelist,self.variable_name = filelist,variable_name
+        self.workingfolder=workingfolder
 
-    #this is dodge...
-    x,y=np.meshgrid(np.arange(np.shape(name_of_array)[2]),np.arange(np.shape(name_of_array)[1]))
-    minvar=np.min(name_of_array)
-    maxvar=np.max(name_of_array)
-    for tstep in np.arange(np.shape(name_of_array)[timedimen]):
-        lg.debug("Working timestep: " + str(tstep))
+    def lights(self):
+        var_timedims=[]
 
-        ax=fig.add_subplot(111)
+        #error checks files, are all similar
+        for f in self.filelist:
+            if not os.path.exists(f):
+                lg.error("Input file: " + str(os.path.basename(f))  + " does not exist.")
+                sys.exit("Input file: " + str(os.path.basename(f))  + " does not exist.")
 
-        ax.set_title(arguments['VARIABLE_NAME'])
-        #ax.set_xlabel('msg')
-        #ax.set_ylabel('msg')
+            ifile=Dataset(f, 'r')
 
-        name_of_array= np.ma.masked_where(
-            name_of_array==0.,
-            name_of_array) 
+            if self.variable_name not in ifile.variables.keys():
+                lg.error("Variable: " + str(self.variable_name) + " does not exist in netcdf4 file.")
+                sys.exit("Variable: " + str(self.variable_name) + " does not exist in netcdf4 file.")
 
-        cs1=plt.contourf(x,y,name_of_array[tstep,:,:],levels=np.linspace(minvar,maxvar,30))
+            #find unlimited dimension
+            findunlim=[ifile.dimensions[dim].isunlimited() for dim in ifile.dimensions.keys()]
+            dim_unlim_num=[i for i, x in enumerate(findunlim) if x]
+            if len(dim_unlim_num)==0:
+                lg.error("Input file: " + str(os.path.basename(file))  + " has no unlimited dimension, which dim is time?")
+                sys.exit("Input file: " + str(os.path.basename(file))  + " has no unlimited dimension, which dim is time?")
+            elif len(dim_unlim_num)>1:
+                lg.error("Input file: " + str(os.path.basename(file))  + " has more than one unlimited dimension.")
+                sys.exit("Input file: " + str(os.path.basename(file))  + " has more than one unlimited dimension.")
+            else:
+                timename=ifile.dimensions.keys()[dim_unlim_num[0]]
+                var_timedim=[i for i, x in enumerate(ifile.variables[self.variable_name].dimensions) if x==timename][0]
+                var_timedims.append(var_timedim)
+            ifile.close()
 
-        #land mask...
-        cs2=ax.contour(x,y,name_of_array[tstep,:,:].mask,levels=[-1,0],linewidths=1,colors='black')
+        #check all unlimited dimensions are in the same place across all files..
+        if var_timedims[1:]==var_timedims[:-1]:
+            self.timedim=var_timedims[0]
+        else:
+            lg.error("(Unlimited) 'time' dimension was not the same across all files, fatal error.")
+            sys.exit("(Unlimited) 'time' dimension was not the same across all files, fatal error.")
+        return
+        
+    def camera(self):
+        """function to create plots.
+        
+        :workingfolder: @todo
+        :returns: @todo
+        """
+        framecnt=1
+        for f in self.filelist:
+            ifile=Dataset(f, 'r')
+            name_of_array=ifile.variables[self.variable_name][:]
 
-        plt.colorbar(cs1)
-        #plt.show()
-    
-        fig.savefig(workingfolder+'/moviepar'+str(tstep).zfill(5)+'.png',dpi=300)
-        #fig.savefig('./.pdf',format='pdf')
-        fig.clf()
-        del ax
-    print workingfolder
+            plt.close('all')
+            fig=plt.figure()
 
-    #ollie's command didn't work on storm
-    #ffmpeg -framerate 10 -y -i plot_%04d.png -s:v 1920x1080 -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p movie.mp4
+            #this is currently dodge...
+            x,y=np.meshgrid(np.arange(np.shape(name_of_array)[2]),np.arange(np.shape(name_of_array)[1]))
 
-    os.chdir(workingfolder)
-    subprocess.call('ffmpeg -r 15 -qscale 3 -y -an -i ' + 'moviepar%05d.png '+os.path.basename(arguments['FILE_NAME']) + '.mov',shell=True)
+            minvar=np.min(name_of_array)
+            maxvar=np.max(name_of_array)
+            for tstep in np.arange(np.shape(name_of_array)[self.timedim]):
+                lg.debug("Working timestep: " + str(framecnt)+ " frames in: " +self.workingfolder)
 
-    #remove png
-    if os.path.isfile(workingfolder+os.path.basename(arguments['FILE_NAME']) + '.mov'):
-        ifiles=sorted(glob.glob(workingfolder + 'moviepar*.png' ))
-        assert(ifiles!=[]),"glob didn't find anything!"
-        for f in ifiles:
-            os.remove(f)
+                ax=fig.add_subplot(111)
 
+                ax.set_title(self.variable_name+' frame num is: ' +str(framecnt))
+                #ax.set_xlabel('msg')
+                #ax.set_ylabel('msg')
+
+                name_of_array= np.ma.masked_where(
+                    name_of_array==0.,
+                    name_of_array) 
+
+                cs1=plt.contourf(x,y,name_of_array[tstep,:,:],levels=np.linspace(minvar,maxvar,30))
+
+                #land mask...
+                cs2=ax.contour(x,y,name_of_array[tstep,:,:].mask,levels=[-1,0],linewidths=1,colors='black')
+
+                plt.colorbar(cs1)
+                #plt.show()
+            
+                fig.savefig(self.workingfolder+'/moviepar'+str(framecnt).zfill(5)+'.png',dpi=300)
+                #fig.savefig('./.pdf',format='pdf')
+                fig.clf()
+                del ax
+                framecnt+=1
+
+    def action(self):
+        """function to stitch the movies together!
+        
+        :returns: @todo
+        """
+
+        #ollie's command didn't work on storm
+        #ffmpeg -framerate 10 -y -i plot_%04d.png -s:v 1920x1080 -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p movie.mp4
+
+        os.chdir(self.workingfolder)
+        subprocess.call('ffmpeg -r 15 -qscale 3 -y -an -i ' + 'moviepar%05d.png '+'movie.mov',shell=True)
+
+        #remove png
+        if os.path.isfile(self.workingfolder+'movie.mov'):
+            ifiles=sorted(glob.glob(self.workingfolder+ 'moviepar*.png' ))
+            assert(ifiles!=[]),"glob didn't find anything!"
+            for f in ifiles:
+                os.remove(f)
+
+            lg.info("MkMov SUCCESS, check it out: "+self.workingfolder+'movie.mov')
+        else:
+            lg.info("MkMov FAIL")
+            lg.error("Something went wrong with ffmpeg, it hasn't made a movie :( We won't delete the plots.")
+            sys.exit("Something went wrong with ffmpeg, it hasn't made a movie :( We won't delete the plots.")
 
 if __name__ == "__main__": 
     LogStart('',fout=False)
@@ -207,39 +292,12 @@ if __name__ == "__main__":
     import matplotlib.pyplot as plt
     import numpy as np
 
-    #file='/path/to/netcdf4/file.nc'
-    file=arguments['FILE_NAME']
-    variable=arguments['VARIABLE_NAME']
+    movmk=MovMaker(arguments['FILE_NAME'],arguments['VARIABLE_NAME'],workingfol)
 
-    #error check for file
-    if not os.path.exists(file):
-        lg.error("Input file: " + str(os.path.basename(file))  + " does not exist.")
-        sys.exit("Input file: " + str(os.path.basename(file))  + " does not exist.")
-
-    ifile=Dataset(file, 'r')
-
-    if variable not in ifile.variables.keys():
-        lg.error("Variable: " + str(variable) + " does not exist in netcdf4 file.")
-        sys.exit("Variable: " + str(variable) + " does not exist in netcdf4 file.")
-    #import ipdb; ipdb.set_trace()
-
-    #find unlimited dimension
-    findunlim=[ifile.dimensions[dim].isunlimited() for dim in ifile.dimensions.keys()]
-    dim_unlim_num=[i for i, x in enumerate(findunlim) if x]
-    if len(dim_unlim_num)==0:
-        lg.error("Input file: " + str(os.path.basename(file))  + " has no unlimited dimension, which dim is time?")
-        sys.exit("Input file: " + str(os.path.basename(file))  + " has no unlimited dimension, which dim is time?")
-    elif len(dim_unlim_num)>1:
-        lg.error("Input file: " + str(os.path.basename(file))  + " has more than one unlimited dimension.")
-        sys.exit("Input file: " + str(os.path.basename(file))  + " has more than one unlimited dimension.")
-    else:
-        timename=ifile.dimensions.keys()[dim_unlim_num[0]]
-        var_timedim=[i for i, x in enumerate(ifile.variables[variable].dimensions) if x==timename][0]
-
-    varone=ifile.variables[variable][:]
-
-    create_plot(varone,var_timedim,workingfol)
-
+    #aah I've always wanted to say this!
+    movmk.lights()
+    movmk.camera()
+    movmk.action()
 
     lg.info('')
     localtime = time.asctime( time.localtime(time.time()) )
