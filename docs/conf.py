85271715535027006b261ac4a24f23a6a8b6327d
Christopher Bull <z3457920@student.unsw.edu.au>
2015-12-27 13:21:46 +1100

    Generated gh-pages for commit 611eda0 Author: Christopher Bull <z3457920@student.unsw.edu.au>
        trying to get readthedocs to work

--- docs/conf.py
+++ docs/conf.py
@@ -29,6 +29,8 @@ import shlex
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+
+sys.path.append(".")  # Needed to find fulltoc
 extensions = ['sphinxcontrib.fulltoc']
 
 # Add any paths that contain templates here, relative to this directory.
