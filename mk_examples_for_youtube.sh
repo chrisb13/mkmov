62a812bc3b8ff882c6c5ab4eaa3f9bc83a4e6077
Christopher Bull <z3457920@student.unsw.edu.au>
2016-03-09 09:02:27 +1100

    made some orca025 examples

--- mk_examples_for_youtube.sh
+++ mk_examples_for_youtube.sh
@@ -32,8 +32,12 @@ source activate root
 
 #python mkmov.py zos --min -1 --max 1 --lmask 0 -o /srv/ccrc/data42/z3457920/mkmovmovies4/1_cordex24_AGRIF-ERAI09_1d_grid_T_2D_zos.mov /srv/ccrc/data42/z3457920/20151012_eac_sep_dynamics/nemo_cordex24_AGRIF_ERAI09/*/1_cordex24_AGRIF-ERAI09_1d_*grid_T_2D.nc &>  /srv/ccrc/data42/z3457920/mkmovmovies4/13.log &
 
-python mkmov.py adt --lmask -214748 -o /srv/ccrc/data42/z3457920/mkmovmovies4/AVISOdt_global_allsat_madt.mov /srv/ccrc/data42/z3457920/RawData/AVISO/RawData/dt_global_allsat_madt/ftp.aviso.altimetry.fr/global/delayed-time/grids/madt/all-sat-merged/h/*/*.nc &>  /srv/ccrc/data42/z3457920/mkmovmovies4/14.log &
+#python mkmov.py adt --lmask -214748 -o /srv/ccrc/data42/z3457920/mkmovmovies4/AVISOdt_global_allsat_madt.mov /srv/ccrc/data42/z3457920/RawData/AVISO/RawData/dt_global_allsat_madt/ftp.aviso.altimetry.fr/global/delayed-time/grids/madt/all-sat-merged/h/*/*.nc &>  /srv/ccrc/data42/z3457920/mkmovmovies4/14.log &
 
-python mkmov.py adt --lmask -214748 --cmap Set3 -o /srv/ccrc/data42/z3457920/mkmovmovies4/AVISOdt_global_allsat_madt_Set3.mov /srv/ccrc/data42/z3457920/RawData/AVISO/RawData/dt_global_allsat_madt/ftp.aviso.altimetry.fr/global/delayed-time/grids/madt/all-sat-merged/h/*/*.nc &>  /srv/ccrc/data42/z3457920/mkmovmovies4/15.log &
+#python mkmov.py adt --lmask -214748 --cmap Set3 -o /srv/ccrc/data42/z3457920/mkmovmovies4/AVISOdt_global_allsat_madt_Set3.mov /srv/ccrc/data42/z3457920/RawData/AVISO/RawData/dt_global_allsat_madt/ftp.aviso.altimetry.fr/global/delayed-time/grids/madt/all-sat-merged/h/*/*.nc &>  /srv/ccrc/data42/z3457920/mkmovmovies4/15.log &
 
-python mkmov.py adt --lmask -214748 --cmap Dark2 -o /srv/ccrc/data42/z3457920/mkmovmovies4/AVISOdt_global_allsat_madt_Dark2.mov /srv/ccrc/data42/z3457920/RawData/AVISO/RawData/dt_global_allsat_madt/ftp.aviso.altimetry.fr/global/delayed-time/grids/madt/all-sat-merged/h/*/*.nc &>  /srv/ccrc/data42/z3457920/mkmovmovies4/16.log &
+#python mkmov.py adt --lmask -214748 --cmap Dark2 -o /srv/ccrc/data42/z3457920/mkmovmovies4/AVISOdt_global_allsat_madt_Dark2.mov /srv/ccrc/data42/z3457920/RawData/AVISO/RawData/dt_global_allsat_madt/ftp.aviso.altimetry.fr/global/delayed-time/grids/madt/all-sat-merged/h/*/*.nc &>  /srv/ccrc/data42/z3457920/mkmovmovies4/16.log &
+
+python mkmov.py  sossheig --cmap Set3 --lmask 0 --min -2.2 --max 1.5 -o /srv/ccrc/data42/z3457920/mkmovmovies4/orca025_Set3.mov /srv/ccrc/data22/z3381502/ORCA025.L75-MJM95-S/*_gridT.nc &>  /srv/ccrc/data42/z3457920/mkmovmovies4/17.log &
+python mkmov.py  sossheig --cmap Dark2 --lmask 0 --min -2.2 --max 1.5 -o /srv/ccrc/data42/z3457920/mkmovmovies4/orca025_Dark2.mov /srv/ccrc/data22/z3381502/ORCA025.L75-MJM95-S/*_gridT.nc &>  /srv/ccrc/data42/z3457920/mkmovmovies4/18.log &
+python mkmov.py  sossheig --cmap Accent --lmask 0 --min -2.2 --max 1.5 -o /srv/ccrc/data42/z3457920/mkmovmovies4/orca025_Accent.mov /srv/ccrc/data22/z3381502/ORCA025.L75-MJM95-S/*_gridT.nc &>  /srv/ccrc/data42/z3457920/mkmovmovies4/19.log &
